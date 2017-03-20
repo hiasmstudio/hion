e18c9b621604a332a098b50d0102923b2f85a55b
Puux <puuxmine@gmail.com>
2017-03-20 22:04:34 +0300

    Show diff in test prejects

--- pack/webapp/tests/run.php
+++ pack/webapp/tests/run.php
@@ -1,4 +1,5 @@
 <?php
+	require_once './class.Diff.php';
 
 	$dir = "./projects/";
 	if ($dh = opendir($dir)) {
@@ -13,19 +14,34 @@
 				//exec("sudo -u ".SUDO_USER." hiasm5 -m $mode $file", $out, $state);
 				exec("hiasm5 -m make-test $dir$file", $out, $state);
 
-				$md5Hash = substr($out[0], 0, 32);
-				$hash = "./hash/$file.md5";
+				$hash = "./hash/$file.js";
+				$index = 1;
+				$code = "";
+				if($out[$index] == "CODEBEGIN") {
+					while($out[++$index] != "CODEEND") {
+						$code .= $out[$index]."\n";
+					}
+				}
 				
-				if($state || file_exists($hash) && file_get_contents($hash) != $md5Hash) {
-					echo join("\n", $out);
+				if($state || file_exists($hash) && ($f = file_get_contents($hash)) != $code) {
+					echo "[\033[1;31mFAILED\033[0m] $file\n";
+					
+					$diff = Diff::compare($f, $code);
+					foreach($diff as $line) {
+						if($line[1]) {
+							echo ($line[1] == 1 ? "-" : "+").$line[0]."\n";
+						}
+					}
+					
+					echo join("\n", array_slice($out, $index+1));
 					if($state)
 						echo "\nExit code: $state\n";
-					echo "[\033[1;31mFAILED\033[0m] $file\n";
 					$failed ++;
+					echo "\n\n";
 				}
 				else {
 					if(!file_exists($hash))
-						file_put_contents($hash, $md5Hash);
+						file_put_contents($hash, $code);
 					echo "[  \033[1;32mOK\033[0m  ] $file\n";
 					$ok ++;
 				}
